9ed8d8782f6e1b1a2d9464e0f31baa02b1964857
buczkotamas <buczkotamas@gmail.com>
2023-01-09 15:54:49 +0100

    init/restore

--- src/main.cpp
+++ src/main.cpp
@@ -5,66 +5,70 @@
 
 #define IR_SENSOR_PIN GPIO_NUM_14
 
-#define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP 5        /* Time ESP32 will go to sleep (in seconds) */
+#define TIME_TO_SLEEP_5s 5 * 1000000
+#define TIME_TO_SLEEP_1h 60 * 60 * 1000000
 // HW-006 v1.3
-#define WAKEUP_ON_ENTER_SILVER_DOT 1
-#define WAKEUP_ON_EXIT_SILVER_DOT 0
+#define PIN_LEVEL_ON_SILVER_DOT 1
+#define PIN_LEVEL_NOT_ON_SILVER_DOT 0
 
 RTC_DATA_ATTR int gas_count = 0;
+RTC_DATA_ATTR int wakeup_level = PIN_LEVEL_ON_SILVER_DOT;
 
 String jsondata;
 StaticJsonDocument<64> doc;
+esp_now_peer_info_t peerInfo;
 
 uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 // uint8_t broadcastAddress[] = {0xBC, 0xDD, 0xC2, 0x88, 0x81, 0x6};
 
-// function for Sending data
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+void esp_now_send_cb(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-void initESPNow()
+esp_err_t init_esp_now()
 {
   WiFi.mode(WIFI_STA);
-
-  // Init ESP-NOW
   if (esp_now_init() != ESP_OK)
   {
     Serial.println("Error initializing ESP-NOW");
-    return;
+    return ESP_FAIL;
   }
-
-  // Register peer
-  esp_now_peer_info_t peerInfo;
-  memset(&peerInfo, 0, sizeof(peerInfo));
-  for (int ii = 0; ii < 6; ++ii)
-  {
-    peerInfo.peer_addr[ii] = (uint8_t)broadcastAddress[ii];
-  }
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
-  // Add peer
   if (esp_now_add_peer(&peerInfo) != ESP_OK)
   {
     Serial.println("Failed to add peer");
-    return;
+    return ESP_FAIL;
   }
-
-  esp_now_register_send_cb(OnDataSent);
+  // esp_now_register_send_cb(esp_now_send_cb);
+  return ESP_OK;
 }
 
-void sendData()
+float get_battery_voltage()
 {
-  initESPNow();
+  return 42;
+}
+
+float get_temperature()
+{
+  return 42;
+}
+
+void send_data()
+{
+  if (init_esp_now() != ESP_OK)
+  {
+    return;
+  }
   jsondata.clear();
   doc.clear();
   doc["_gas"] = gas_count;
-  doc["_temp"] = 2;
-  doc["_batt"] = 3;
-  serializeJson(doc, jsondata);                                                   // Serilizing JSON
+  doc["_temp"] = get_temperature();
+  doc["_batt"] = get_battery_voltage();
+  serializeJson(doc, jsondata); // Serilizing JSON
   Serial.println(jsondata);
   esp_now_send(broadcastAddress, (uint8_t *)jsondata.c_str(), jsondata.length()); // Sending "jsondata"
 }
@@ -72,61 +76,34 @@ void sendData()
 void setup()
 {
   Serial.begin(115200);
-
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-
-  esp_sleep_enable_ext0_wakeup(IR_SENSOR_PIN, WAKEUP_ON_ENTER_SILVER_DOT); // 1 = High, 0 = Low
-
-  esp_sleep_wakeup_cause_t wakeup_reason;
-
-  wakeup_reason = esp_sleep_get_wakeup_cause();
-
-  switch (wakeup_reason)
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP_5s);
+  if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0)
   {
-  case ESP_SLEEP_WAKEUP_EXT0:
-    Serial.println("Wakeup caused by external signal using RTC_IO");
-    gas_count++;
-    sendData();
-    break;
-  case ESP_SLEEP_WAKEUP_EXT1:
-    Serial.println("Wakeup caused by external signal using RTC_CNTL");
-    break;
-  case ESP_SLEEP_WAKEUP_TIMER:
-    Serial.println("Wakeup caused by timer");
-    sendData();
-    Serial.println("GO TO SLEEP...");
-    esp_deep_sleep_start();
-    break;
-  case ESP_SLEEP_WAKEUP_TOUCHPAD:
-    Serial.println("Wakeup caused by touchpad");
-    break;
-  case ESP_SLEEP_WAKEUP_ULP:
-    Serial.println("Wakeup caused by ULP program");
-    break;
-  default:
-    Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
-    break;
+    if (wakeup_level == PIN_LEVEL_ON_SILVER_DOT)
+    {
+      gas_count++;
+    }
   }
+  send_data();
 }
 
 void loop()
 {
+  wakeup_level = PIN_LEVEL_NOT_ON_SILVER_DOT;
   for (int i = 0; i < 5; i++)
   {
-    int ir_pin = digitalRead(IR_SENSOR_PIN);
-    if (ir_pin == 1)
+    int sensor_pin = digitalRead(IR_SENSOR_PIN);
+    if (sensor_pin == PIN_LEVEL_ON_SILVER_DOT)
     {
-      delay(1000);
+      delay(1000); // silver dot still visible
     }
     else
     {
-      Serial.println("GO TO SLEEP...");
-      esp_deep_sleep_start();
+      wakeup_level = PIN_LEVEL_ON_SILVER_DOT;
+      break;
     }
   }
-  Serial.println("STOPPED ON SILVER DOT -> CHANGE WAKEUP LEVEL");
-  gas_count--; // on wakeup gas_count++ going to be called
-  esp_sleep_enable_ext0_wakeup(IR_SENSOR_PIN, WAKEUP_ON_EXIT_SILVER_DOT); // 1 = High, 0 = Low
-  Serial.println("GO TO SLEEP...");
+  Serial.printf("Go to sleep, wakeup on %d\n", wakeup_level);
+  esp_sleep_enable_ext0_wakeup(IR_SENSOR_PIN, wakeup_level);
   esp_deep_sleep_start();
 }
